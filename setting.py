64a6bfc95c6458563dbe8c75046ea101ddc4a111
panfeng <975713384@qq.com>
2019-04-18 15:11:59 +0800

    test pha

--- setting.py
+++ setting.py
@@ -3,4 +3,3 @@ import os
 
 class Setting:
     ROOT_PATH = os.path.abspath(os.path.dirname(__file__))
-
--- test/test_yaml.py
+++ test/test_yaml.py
@@ -4,9 +4,9 @@ from util.operate_yaml import OperateYAML
 def test_get_data():
     _yml = OperateYAML(yaml_path='test.yml')
     try:
-        res1 = _yml.get_data('a', 'aa')
+        _yml.get_data('a', 'aa')
     except RuntimeError as re:
         assert re.args[0] == '元素格式错误'
 
-    res2 = _yml.get_data('a', 'bb')
-    assert res2 == ('id', 'bbb')
+    res = _yml.get_data('a', 'bb')
+    assert res == ('id', 'bbb')
